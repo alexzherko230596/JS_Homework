e059faf2d5d09c4f2a2b878d9d87ab22d6ae152f
Alex <alexzherko@MacBook-Air-Alex.local>
2021-08-29 15:52:41 +0200

    Homework 9 done

--- Lessons-9/task5.js
+++ Lessons-9/task5.js
@@ -1,11 +1,12 @@
 var arr = [1,2,3,4,5,6,7];
+var sub = [];
 function slised(arr, number){
-    var sub = [];
     while(arr.length > 0){
         sub.push(arr.splice(0, number));
     }
 
-    return console.log(sub);
+    return sub;
 }
 
 slised(arr, 2);
+console.log(sub);
